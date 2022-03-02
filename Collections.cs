53c83db662e73860350d6027fc905211d0211941
Sawfeeq-Mohammed <Sawfeeq.Mohammed@ibm.com>
2022-03-02 17:08:51 +0530

    Day 3 Task 2

---
+++ Collections.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyirstConsole
+{
+    internal class Collections
+    {
+        static void Main(string[] args)
+        {
+            list<int> mylist = new list<int>();
+
+            mylist.add(10);
+            mylist.add(20);
+            mylist.add(30);
+            mylist.add(40);
+
+            foreach (int i in mylist)
+            {
+                console.writeline(i);
+            }
+            //linkedlist<string> mylist = new linkedlist<string>();
+
+
+
+            //mylist.addfirst("war");
+            //mylist.addfirst("the");
+            //mylist.addfirst("stop");
+            //mylist.addfirst("please");
+
+            //if (mylist.count > 0)
+            //{
+            //    foreach (string str in mylist)
+            //    {
+            //        console.writeline(str);
+            //    }
+            //}
+            //else
+            //{
+            //    console.writeline("the list is empty");
+            //}
+            //ArrayList Mylist = new ArrayList();
+
+            //Console.WriteLine("Before insertion", Mylist.Count);
+
+            //Mylist.Add("List");
+            //Mylist.Add(10);
+            //Mylist.Add(true);
+            //Mylist.Add(10.00);
+
+            //foreach (ArrayList i in Mylist)
+            //{
+            //    Console.WriteLine(i);
+
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
